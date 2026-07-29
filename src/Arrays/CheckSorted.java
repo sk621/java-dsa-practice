54b15ffef9480964e742b8938c1e0254570b50d7
sk621 <shivendukumarsingh621@gmail.com>
2026-07-29 21:42:54 +0530

    style: format updated

--- src/Arrays/CheckSorted.java
+++ src/Arrays/CheckSorted.java
@@ -18,7 +18,6 @@ public class CheckSorted {
     static boolean checkArray(int[] arr) {
 
         for(int i=0;i<arr.length-1;i++){
-
             if(arr[i]>arr[i+1]){
                 return false;
             }
