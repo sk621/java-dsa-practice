7aa5e0220224fe05fa49a40b1257607299a1de42
sk621 <shivendukumarsingh621@gmail.com>
2026-08-01 00:44:03 +0530

    feat: add print subsequences with sum k

---
+++ src/Recursion/SubsequenceSum.java
@@ -0,0 +1,58 @@
+//WAP to print subsequence with sum k
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SubsequenceSum {
+    static Scanner scn = new Scanner(System.in);
+
+    static void inputArray(int[] arr){
+
+        for(int i=0; i<arr.length; i++){
+
+            System.out.print("Enter element " + i +": ");
+
+            arr[i] = scn.nextInt();
+        }
+    }
+
+    static void printSub(int[] arr, int i, ArrayList<Integer> list,int res,int sum){
+
+        if(i >= arr.length){
+            if(res == sum){
+                System.out.print(list + " ");
+            }
+            return;
+        }
+
+        list.add(arr[i]);
+        res += arr[i];
+        printSub(arr,i+1,list,res,sum);
+
+        list.remove(Integer.valueOf(arr[i]));
+        res -= arr[i];
+        printSub(arr,i+1,list,res,sum);
+    }
+
+    static void main(String[] args){
+
+        System.out.print("Enter size of the array: ");
+
+        int size = scn.nextInt();
+
+        int[] arr = new int[size];
+
+        inputArray(arr);
+
+        ArrayList <Integer> list = new ArrayList<>();
+
+        System.out.print("Enter a sum: ");
+
+        int sum = scn.nextInt();
+
+        System.out.println("All the subsequences with sum "+ sum + " of the array are:");
+
+        printSub(arr,0,list,0,sum);
+    }
+}
