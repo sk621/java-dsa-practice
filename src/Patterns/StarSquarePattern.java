8612fa0cfc7fea97538311cbe27e8385c60634ce
sk621 <shivendukumarsingh621@gmail.com>
2026-08-01 23:57:40 +0530

    feat: add a star square pattern

---
+++ src/Patterns/StarSquarePattern.java
@@ -0,0 +1,30 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class StarSquarePattern {
+    static Scanner scn = new Scanner(System.in);
+
+    static void printPattern(int val){
+
+        for(int i=0;i<val;i++){
+
+            for(int j=0;j<val;j++){
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+
+        System.out.print("Enter a value: ");
+
+        int val = scn.nextInt();
+
+        System.out.print("The star square pattern is:");
+
+        printPattern(val);
+    }
+}
