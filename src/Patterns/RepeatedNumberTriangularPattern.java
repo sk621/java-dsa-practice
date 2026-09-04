155c88f9362991943ecd3fc69dc1bdffeb903324
sk621 <shivendukumarsingh621@gmail.com>
2026-09-04 20:19:16 +0530

    feat: add a pattern program

--- src/Patterns/RepeatedNumberTriangularPattern.java
+++ src/Patterns/RepeatedNumberTriangularPattern.java
@@ -1,4 +1,30 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class RepeatedNumberTriangularPattern {
+    static Scanner scn = new Scanner(System.in);
+
+    static void printPattern(int val){
+
+        for(int i = 1; i <= val; i++){
+
+            for(int j = 1; j <= i; j++){
+
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+
+        System.out.print("Enter a value: ");
+
+        int val = scn.nextInt();
+
+        System.out.println("The star square pattern is:");
+
+        printPattern(val);
+    }
 }
