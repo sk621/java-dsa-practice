1681c6195aa52f4fdb291b1fedbacf8fed9485fe
sk621 <shivendukumarsingh621@gmail.com>
2026-09-04 20:31:00 +0530

    feat: add a pattern program

---
+++ src/Patterns/InvertedTriangleNumberPattern.java
@@ -0,0 +1,30 @@
+package Patterns;
+
+import java.util.Scanner;
+public class InvertedTriangleNumberPattern {
+    static Scanner scn = new Scanner(System.in);
+
+    static void printPattern(int val){
+
+        for(int i = 1; i <= val; i++){
+
+            for(int j = 1; j <= val-i+1; j++){
+
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+
+        System.out.print("Enter a value: ");
+
+        int val = scn.nextInt();
+
+        System.out.println("The star square pattern is:");
+
+        printPattern(val);
+    }
+
+}
